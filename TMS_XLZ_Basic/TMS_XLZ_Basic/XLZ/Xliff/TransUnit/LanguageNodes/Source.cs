62448b0496c8c109a436cd7607d1d57c99ea87a0
Aleksander Parol <40210140+alekparol@users.noreply.github.com>
2020-05-08 16:28:07 +0200

    Implementing Doubly Linked Lists

--- TMS_XLZ_Basic/TMS_XLZ_Basic/XLZ/Xliff/TransUnit/LanguageNodes/Source.cs
+++ TMS_XLZ_Basic/TMS_XLZ_Basic/XLZ/Xliff/TransUnit/LanguageNodes/Source.cs
@@ -20,238 +20,96 @@ namespace TMS_XLZ_Basic
 
         /* Fields */
 
-        private XmlNode sourceNode;
-        private XmlNode targetNode;
+        private XmlNode xmlNode;
 
-        private TransUnitElements transUnitElements;
+        private string outerXml;
+        private string innerXml;
+        private string innerText;
 
-        public string outerXml;
-        public string innerXml;
-
-        public int ID;
-        public bool translatable;
-
-        public int validationResult;
+        private TransUnitElements sourceElements;
+        private bool parsingSuccess;
 
         /*Properties*/
 
+        public XmlNode XmlNode
+        {
+            get
+            {
+                return xmlNode;
+            }
+        }
 
+
+        public string OuterXml
+        {
+            get
+            {
+                return outerXml;
+            }
+        }
+
+        public string InnerXml
+        {
+            get
+            {
+                return innerXml;
+            }
+        }
+
+        public string InnerText
+        {
+            get
+            {
+                return innerText;
+            }
+        }
+
+        public TransUnitElements SourceElements
+        {
+            get
+            {
+                return sourceElements;
+            }
+        }
+
+        public bool ParsingSuccess
+        {
+            get
+            {
+                return parsingSuccess;
+            }
+        }
 
         /* Methods */
 
-        public int GetTransUnitValidationResults()
-        {
-            return validationResult;
-        }
-
-        /* Functions for Get source and target Xml nodes.*/
-
-        public XmlNode GetSourceNode()
-        {
-            return sourceNode;
-        }
-
-        public XmlNode GetTargetNode()
-        {
-            return targetNode;
-        }
-
-        /* Functions for Get source and target Xml nodes' text. */
-
-        public string GetSourceText()
-        {
-            return sourceNode.InnerText;
-        }
-
-        public string GetTargetText()
-        {
-            return targetNode.InnerText;
-        }
-
-        /* Functions for Get source and target Xml nodes' Xml code. */
-
-        public string GetSourceXml()
-        {
-            return sourceNode.InnerXml;
-        }
-
-        public string GetTargetXml()
-        {
-            return targetNode.InnerXml;
-        }
-
-        /* Functions for Get source and target Xml nodes' inner Xml code without translatable text. */
-
-        public string GetInnerXmlWithoutText(XmlNode sourceOrTargetNode)
-        {
-
-            string unionOfMatches = "";
-            string innerTextOfTheNode = sourceOrTargetNode.InnerXml;
-
-            /* This regex will match:
-            * - <ept></ept> tags in content.xlf,
-            * - <bpt></bpt> tags in content.xlf,
-            * - <ph></ph> tags in content.xlf
-            * And all the content which is in the scope of those tags and their attributes as well.*/
-
-            Regex rx = new Regex("(<ept.*?>.*</?ept.*?>)|(<bpt.*?>.*</?bpt.*?>)|(<ph.*?>.*</?ph.*?>)|(<it.*?>.*</?it.*?>)");
-            MatchCollection matches = rx.Matches(innerTextOfTheNode);
-
-            foreach (Match en in matches)
-            {
-                unionOfMatches = unionOfMatches + en.Value;
-            }
-
-            return unionOfMatches;
-        }
-
-        public string GetSourceInnerXmlWithoutText()
-        {
-            return GetInnerXmlWithoutText(sourceNode);
-        }
-
-        public string GetTargetInnerXmlWithoutText()
-        {
-            return GetInnerXmlWithoutText(targetNode);
-        }
-
-        public string GetInnerTextWithoutXml(XmlNode sourceOrTargetNode)
-        {
-
-            string setminusOfMatches = "";
-            string innerTextOfTheNode = sourceOrTargetNode.InnerXml;
-
-            Regex rx = new Regex("(<ept.*?>.*</?ept.*?>)|(<bpt.*?>.*</?bpt.*?>)|(<ph.*?>.*</?ph.*?>)");
-            setminusOfMatches = rx.Replace(innerTextOfTheNode, string.Empty);
-
-
-            return setminusOfMatches;
-
-        }
-
-        public string GetSourceInnerTextWithoutXml()
-        {
-            return GetInnerTextWithoutXml(sourceNode);
-        }
-
-        public string GetTargetInnerTextWithoutXml()
-        {
-            return GetInnerTextWithoutXml(targetNode);
-        }
-
-        /* Functions for Get source and target Xml nodes' outer Xml code without translatable text. */
-
         public string GetSourceOuterXmlWithoutText()
         {
-            string sourceText = sourceNode.InnerText;
-            string xmlWithoutText = sourceNode.OuterXml.Replace(sourceText, "");
+            string sourceText = xmlNode.InnerText;
+            string xmlWithoutText = xmlNode.OuterXml.Replace(sourceText, "");
 
             return xmlWithoutText;
-        }
-
-        public string GetTargetOuterXmlWithoutText()
-        {
-            string targetText = targetNode.InnerText;
-            string xmlWithoutText = targetNode.OuterXml.Replace(targetText, "");
-
-            return xmlWithoutText;
-        }
-
-        /* Function that returns boolean value responsible for translation. */
-
-        public bool IsTranslatable()
-        {
-            return translatable;
-        }
-
-        /* Function that returns ID value for trans-unit node. */
-
-        public int GetTransUnitID()
-        {
-            return ID;
-        }
-
-        /* Experimental methods */
-
-        public void GetBPT()
-        {
-
-        }
-
-        public void GetEPT()
-        {
-
-        }
-
-        public void GetPH()
-        {
-
-        }
-
-        public void GetTextBPTID(int BPTID)
-        {
-            /*<bpt id="x"></bpt>We are talking abouth this text<ept id="x"></ept>*/
         }
 
         /* Constructors */
 
-        public Source(XmlNode transUnitNode)
+        public Source(XmlNode xmlNodeContainingSource)
         {
 
-            /* Initializing transUnitNode field. */
-
-
-            /* Initializing previous syblings fields. */
-
-            /* Initializing ID field. */
-
-            bool success = Int32.TryParse(transUnitNode.Attributes["id"].Value, out int transUnitID);
-
-            if (success)
+            if (xmlNodeContainingSource.SelectSingleNode("./source") != null)
             {
-                ID = transUnitID;
-            }
-            else
-            {
-                ID = -1;
+                parsingSuccess = true;
+                xmlNode = xmlNodeContainingSource.SelectSingleNode("./source");
             }
 
-            /* Initializing translatable field. */
+            outerXml = xmlNode.OuterXml;
+            innerXml = xmlNode.InnerXml;
+            innerText = xmlNode.InnerText;
 
-            if (transUnitNode.Attributes["translate"].Value == "no")
+            if(innerXml.Length != 0)
             {
-                translatable = false;
-            }
-            else if (transUnitNode.Attributes["translate"].Value == "yes")
-            {
-                translatable = true;
+                sourceElements = new TransUnitElements(innerXml);
             }
 
-
-            /* Initializing sourceNode and targetNode fields. */
-
-            if (transUnitNode.SelectSingleNode("./source") != null)
-            {
-                validationResult += 1;
-                sourceNode = transUnitNode.SelectSingleNode("./source");
-            }
-            else
-            {
-                validationResult -= 2;
-            }
-
-            if (transUnitNode.SelectSingleNode("./target") != null)
-            {
-                validationResult += 1;
-                targetNode = transUnitNode.SelectSingleNode("./target");
-            }
-            else
-            {
-                validationResult -= 1;
-            }
-
-            outerXml = transUnitNode.OuterXml;
-            innerXml = transUnitNode.InnerXml;
         }
     }
 }
---
+++ TMS_XLZ_Basic/TMS_XLZ_Basic/XLZ/Xliff/TransUnit/TransUnitNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using TMS_XLZ_Basic.XLZ.Xliff.TransUnit.TransUnitElements;
+
+
+namespace TMS_XLZ_Basic
+{
+    class TransUnitNode
+    {
+
+        /* Fields */
+
+        private TransUnitData data;
+
+        private TransUnitNode nextSibling;
+        private TransUnitNode previousSibling;
+
+        /*Properties*/
+
+
+
+        /* Methods */
+
+        /* Constructors */
+
+        public TransUnitNode(XmlNode transUnitNode)
+        {
+
+            data = new TransUnitData(transUnitNode);
+
+            nextSibling = new TransUnitNode(transUnitNode.NextSibling);
+            previousSibling = new TransUnitNode(transUnitNode.PreviousSibling);
+
+        }
+
+    }
+}
